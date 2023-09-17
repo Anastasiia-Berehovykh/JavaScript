e6dd2536ec87dbc15c9a855d943713afce984a07
Anastasiia-Berehovykh <anastanber@gmail.com>
2023-09-17 12:34:04 +0300

    Lesson1_1

---
+++ script.js
@@ -0,0 +1,45 @@
+console.log('Hello world');
+
+const apple = 10;
+const orange = 6;
+const sum = apple + orange;
+console.log(sum);
+const minus = apple - orange;
+console.log(minus);
+let students = 50;
+students += 35;
+students = students +35;
+console.log(students);
+const value = 27.8;
+console.log(Math.floor(value));
+console.log(Math.ceil(value));
+console.log(Math.round(value));
+const names = "Anna";
+const classes = 5;
+const message = `${names} study at ${classes}`;
+console.log(message);
+let weight = "88,3";
+let height = "1.75";
+weight = weight.replace(",", ".");
+weight = Number(weight);
+height = Number(height);
+
+console.log(weight);
+console.log(height);
+console.log(Number.parseFloat(height));
+console.log(Math.pow(height, 2));
+const res = Number((weight / Math.pow(height, 2)).toFixed(1));
+console.log(res);
+
+
+let sum1 = "2,56";
+sum1 = sum1.replace(",", ".");
+console.log(sum1);
+
+
+
+
+
+
+
+
